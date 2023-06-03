f4ec333492d369108f0ba7dd865449d31baa6dcf
wuyiwuyaya <42758160+wuyiwuyaya@users.noreply.github.com>
2023-06-04 01:33:33 +0800

    Update defaults.h

--- trunk/user/shared/defaults.h
+++ trunk/user/shared/defaults.h
@@ -32,7 +32,7 @@
 
 #define DEF_LAN_ADDR		"192.168.1.5"
 #define DEF_LAN_DHCP_BEG	"192.168.1.100"
-#define DEF_LAN_DHCP_END	"192.168.1.244"
+#define DEF_LAN_DHCP_END	"192.168.1.254"
 #define DEF_LAN_MASK		"255.255.255.0"
 
 #define DEF_WLAN_2G_CC		"CN"
